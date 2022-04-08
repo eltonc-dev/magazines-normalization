82ba2a99e70bcc09d260dd8b703581d53b1ce207
eltonc-dev <eltonc.dev@gmail.com>
2022-04-08 09:51:01 -0300

    fetch on module start

--- src/app/magazine/magazine.module.ts
+++ src/app/magazine/magazine.module.ts
@@ -1,21 +1,36 @@
+import { MagazineFacadeService } from './store/facade/magazine-facade.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FetchMagazinesDirective } from './directives/fetch-magazines.directive';
 import { MagazineStoreModule } from './store/magazine-store.module';
+import { MagazineComponent } from './components/magazine/magazine.component';
+import { MagazineListContainerComponent } from './containers/magazine-list-container/magazine-list-container.component';
+import { MagazineIssueComponent } from './components/magazine-issue/magazine-issue.component';
+import { MagazineArticleComponent } from './components/magazine-article/magazine-article.component';
 
 
 
 @NgModule({
   declarations: [
-    FetchMagazinesDirective
+    FetchMagazinesDirective,
+    MagazineComponent,
+    MagazineListContainerComponent,
+    MagazineIssueComponent,
+    MagazineArticleComponent
   ],
   imports: [
     CommonModule,
     MagazineStoreModule
   ],
   exports: [
-    FetchMagazinesDirective
+    FetchMagazinesDirective,
+    MagazineListContainerComponent
   ]
 })
-export class MagazineModule { }
+export class MagazineModule {
+  // Initialize magazine items in the store
+  constructor( private magazinesFacade: MagazineFacadeService){
+    this.magazinesFacade.fetchAll();
+  }
+ }
